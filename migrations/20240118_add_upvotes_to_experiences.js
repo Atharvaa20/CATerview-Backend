b2e9da79d722d2f00d979fc609db0c2b26463a0f
Atharvaa20 <guravatharv2003@gmail.com>
2026-01-18 18:36:31 +0530

    exp colm issue solved

--- migrations/20240118_add_upvotes_to_experiences.js
+++ migrations/20240118_add_upvotes_to_experiences.js
@@ -1,17 +1,17 @@
 'use strict';
 
 module.exports = {
-    up: async (queryInterface, Sequelize) => {
-        // Using raw SQL because the custom simple-migrate.js runner only provides the .query() method
+    up: async (migrationUtils, Sequelize) => {
+        // Using migrationUtils.sequelize.query to be consistent with existing migrations
         console.log('Adding upvotes column...');
-        await queryInterface.query('ALTER TABLE interview_experiences ADD COLUMN IF NOT EXISTS upvotes INTEGER DEFAULT 0 NOT NULL');
+        await migrationUtils.sequelize.query('ALTER TABLE interview_experiences ADD COLUMN IF NOT EXISTS upvotes INTEGER DEFAULT 0 NOT NULL');
 
         console.log('Adding upvoted_by column...');
-        await queryInterface.query('ALTER TABLE interview_experiences ADD COLUMN IF NOT EXISTS upvoted_by JSONB DEFAULT \'[]\'::jsonb NOT NULL');
+        await migrationUtils.sequelize.query('ALTER TABLE interview_experiences ADD COLUMN IF NOT EXISTS upvoted_by JSONB DEFAULT \'[]\'::jsonb NOT NULL');
     },
 
-    down: async (queryInterface, Sequelize) => {
-        await queryInterface.query('ALTER TABLE interview_experiences DROP COLUMN IF EXISTS upvotes');
-        await queryInterface.query('ALTER TABLE interview_experiences DROP COLUMN IF EXISTS upvoted_by');
+    down: async (migrationUtils, Sequelize) => {
+        await migrationUtils.sequelize.query('ALTER TABLE interview_experiences DROP COLUMN IF EXISTS upvotes');
+        await migrationUtils.sequelize.query('ALTER TABLE interview_experiences DROP COLUMN IF EXISTS upvoted_by');
     }
 };
